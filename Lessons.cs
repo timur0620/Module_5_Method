e63719826fb70adbb5b673dd48e26405191febd4
Timur <timur0620@list.ru>
2024-01-31 18:06:35 +0300

    Добавьте файлы проекта.

---
+++ Lessons.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Module_5_Method
+{
+    internal class Lessons
+    {
+        static void Work(string[] args)
+        {
+            IsTesting(100);
+            IsTesting(100, 200);
+            IsTesting(10, 20, 30);
+            IsTesting("string", 10, 20, 30);
+
+            //Infinity();
+            NotInfinity(5);
+
+
+        }
+        static int IsTesting(int num)
+        {
+            return num;
+        }
+        static int IsTesting(int num, int num_1)
+        {
+            return num + num_1;
+        }
+        static int IsTesting(params int[] Args)
+        {
+            int result = 0;
+            foreach (int i in Args)
+            {
+                result += i;
+            }
+            return result;
+        }
+        static string IsTesting(string str, params int[] Args)
+        {
+            int result = 0;
+            foreach (int i in Args)
+            {
+                result += i;
+            }
+            return result.ToString() + str;
+        }
+
+        static void Infinity()
+        {
+            Console.Write("0_-");
+            Thread.Sleep(100);
+            Infinity();
+        }
+        static void NotInfinity(int count)
+        {
+            Console.Write("0_-");
+            Thread.Sleep(100);
+            count--;
+            if (count > 0)
+            {
+                NotInfinity(count);
+            }
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Bogus;
+
+namespace Module_5_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var faker = new Faker();
+            string someString = faker.Lorem.Lines(1);
+
+            if (ScreenText(GetFakeString(someString)).Equals("Finish"))
+            {
+                Console.WriteLine("_______________________________");
+                ScreenText(ReversWorld(GetFakeString(someString)));
+            }              
+        }
+        static string[] GetFakeString(string text)
+        {            
+            string[] result = text.Split(" ");
+
+            return result;
+        }
+        static string ScreenText(string[] text)
+        { 
+            foreach (string s in text)
+            {
+                Console.WriteLine(s);
+                Thread.Sleep(1000);
+            }
+            return "Finish";
+        }
+        static string[] ReversWorld(string[] text)
+        {
+            string[] result = new string[text.Length];
+            int count = 0;
+
+            for (int i = text.Length -1; i > -1; i--)
+            {           
+                result[i] = text[count];
+
+                count++;     
+            }
+            return result;
+        }
+    }
+}
